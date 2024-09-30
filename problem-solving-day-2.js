1bf15759418caffe86b671d9560d79976ff8b4be
Rifat7432 <mdr341762@gmail.com>
2024-09-30 19:54:40 +0600

    day 2 v2

--- problem-solving-day-2.js
+++ problem-solving-day-2.js
@@ -1,7 +1,6 @@
 //6. Task: Array Reduction
 // Create an array of numbers. Write a function that uses the reduce method to calculate the sum of all even numbers in the array.
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const sumOfEvens = (arr) =>
   arr.reduce((sum, num) => (num % 2 === 0 ? sum + num : sum), 0);
 
@@ -11,8 +10,8 @@ console.log(sumOfEvens(array));
 // Write a function that determines whether a given year is a leap year.
 const isLeapYear = year => (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 
-console.log(isLeapYear(2024)); // Output: true
-console.log(isLeapYear(2023)); // Output: false
+console.log(isLeapYear(2024)); 
+console.log(isLeapYear(2023));
 
 //8. Task: Count Vowels:
 // Write a function that counts the number of vowels in a given string.
@@ -22,7 +21,7 @@ const countVowels = str => {
     return [...str].filter(char => vowels.includes(char)).length;
 };
 
-console.log(countVowels('Happy New Year')); // Output: 5
+console.log(countVowels('Happy New Year')); 
 
 //9. Task: Unique Values
 // Create an array of numbers with some duplicate values. Write a function to filter out the duplicate values and return a new array with only unique numbers. Print the result.
@@ -33,11 +32,11 @@ const uniqueValues = (arr) =>{
         array.indexOf(item) === index})
    }
 
-console.log(uniqueValues(numbers)); // Output: [1, 2, 3, 4, 5, 6]
+console.log(uniqueValues(numbers)); 
 
 
 //10. Task: Find Maximum Value:
 // Write a function that takes an array of numbers and returns the maximum value.
 const findMaxValue = arr => Math.max(...arr);
 
-console.log(findMaxValue([1, 2, 3, 10, 4, 5])); // Output: 10
+console.log(findMaxValue([1, 2, 3, 10, 4, 5])); 
